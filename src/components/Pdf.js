d012acc043db850d87acd715af6ad84d3919dd33
hugotata78 <hugotatarinoff@gmail.com>
2021-11-29 21:34:54 -0300

    componente pdf

---
+++ src/components/Pdf.js
@@ -0,0 +1,29 @@
+import React, { useEffect } from 'react'
+import pdf from '../docs/CV-HUGO-TATARINOFF.pdf'
+
+const Pdf = () => {
+
+    useEffect(()=>{
+        if(typeof window.orientation !== "undefined"){
+            document.getElementById('enlaceDescargarPdf').click();
+            window.close();
+        }
+    })
+    return (
+        <div className='pdf'>
+            <object
+                data={pdf}
+                type='application/pdf'
+                width="100%"
+                height="100%"
+            >
+                <br />
+                    <a href={pdf} id="enlaceDescargarPdf"
+                    download="ReactJS.pdf"
+                    >No puedes visualizar el CV, da click aquí para descargarlo</a>
+            </object>
+        </div>
+    )
+}
+
+export default Pdf
