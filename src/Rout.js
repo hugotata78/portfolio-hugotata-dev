d70b6b617dbb4426c05a31fce849adcf1fa8ea97
hugotata78 <hugotatarinoff@gmail.com>
2021-12-27 15:22:10 -0300

    Solucionado despliegue cv.

--- src/Rout.js
+++ src/Rout.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
-import Pdf from './components/Pdf'
 import { Home } from './pages/Home'
 
 const Rout = () => {
     return (
         <Routes>
             <Route path='/' element={ <Home />} />
-            <Route path='/pdf' element={ <Pdf />} />
         </Routes>
     )
 }
